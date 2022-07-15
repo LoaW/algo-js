86deccf16b9d9b792497731bff7f83cae05acaa3
Xavier Brigode <xavier.brigode@gmail.com>
2022-07-15 12:28:58 +0200

    3.0 + 3.1

---
+++ 3.0.js
@@ -0,0 +1,15 @@
+let arr = ["Julien", "Vincent", "Rinaldo", "Sophie", "Quentin", "Xavier", "Titouan", "Julien", "Stephanie", "Eddy" ]
+
+for (let name of arr) {
+    console.log("Hello " + name)
+}
+
+
+
+/*
+let arr = ["cherry", "banana", "orange"]
+
+for (let i = 0; i < arr.length; i++) {
+    console.log("u want some " + arr[i])
+}
+*/
---
+++ 3.1.js
@@ -0,0 +1,8 @@
+let arr = [100,101,102]
+// let arr = [1, 2, 3, 4, 5];
+let res = 0;
+
+for (let number of arr) {
+    res = res + number;
+}
+ console.log(res)
