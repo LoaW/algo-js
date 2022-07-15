08cff6baf3f51b6df2219095b47e8c05d939c05e
Xavier Brigode <xavier.brigode@gmail.com>
2022-07-15 10:29:26 +0200

    + 2.6

---
+++ 2.6.js
@@ -0,0 +1,26 @@
+let number = prompt ("Entrer un numéro compris entre 1 et 7");
+
+if (number == 1){
+    console.log("Nous sommes lundi");
+}
+else if(number == 2){
+    console.log("Nous sommes mardi")
+}
+else if(number == 3){
+    console.log("Nous sommes mercredi")
+}
+else if(number == 4){
+    console.log("Nous sommes jeudi")
+}
+else if(number == 5){
+    console.log("Nous sommes vendredi")
+}
+else if(number == 6){
+    console.log("Nous sommes samedi")
+}
+else if(number == 7){
+    console.log("Nous sommes dimanche")
+}
+else{
+    console.log("Entre un 1 et 7 on a dit")
+}
