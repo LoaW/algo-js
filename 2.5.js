8d0867e9f09feed7a45cba0ed71a24bbf1cc894e
Xavier Brigode <xavier.brigode@gmail.com>
2022-07-15 10:21:55 +0200

    2.5 galère entre !== et != résolue

---
+++ 2.5.js
@@ -0,0 +1,27 @@
+let number = parseInt(prompt("Entrer votre chiffre favori : "));
+
+if (number !== 42 ) {
+    while (number != 42) {
+        number = parseInt(prompt("are u sur ?"));
+    }
+}
+else{
+    console.log("good")
+}
+
+
+
+
+/*
+if (number !== 42) {
+    while (number !== 42) {
+        number = prompt("Ceci n'est pas votre chiffre favori, entrer à nouveau votre chiffre favori : ");
+    }
+}
+else{
+    console.log("Point commun");
+}
+*/
+
+
+
