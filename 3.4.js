f5e9fd93e20edb780a704e3774fe31a3f51cf485
Xavier Brigode <xavier.brigode@gmail.com>
2022-07-19 09:31:08 +0200

    refresh

---
+++ 3.4.js
@@ -0,0 +1,4 @@
+let arr = [2, 5, 4, 9, 6, 3, 7]
+
+console.log(arr[0]);
+console.log(arr[arr.length - 1]);
---
+++ 4.6.js
@@ -0,0 +1,10 @@
+function factorial(a) {
+    if (a < 0) 
+          return -1
+    else if (a == 0) 
+        return 1
+    else {
+        return (a * factorial(a - 1))
+    }
+  }
+  factorial(5)
